ea5e2a63f84acdb01c633b01e59281e3969fb945
SAMSUNG <kki4485@naver.com>
2021-05-07 18:24:30 +0900

    과제2

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
-포켓몬1={"이름":"피카츄","타입":"전기","공격력":55}
-print(포켓몬1["이름"])
-print(포켓몬1["공격력"])
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def info(self):
+        print(f'이름: {self.name}')
+        print(f'나이: {self.age}')
+        print()
+
+student1=student('김주희',22)
+student2=student('박주희',25)
+student3=student('최주희',21)
+student4=student('이주희',23)
+
+student1.info()
+student2.info()
+student3.info()
+student4.info()
+
